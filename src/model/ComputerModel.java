cc4c468f27fcb773dbc8cdb837b32d3287b48e5d
tgrenier <tgrenier@excilys.com>
2015-03-12 17:33:10 +0100

    ajout model

---
+++ src/model/ComputerModel.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.util.Date;
+
+public class ComputerModel {
+
+	private long id;
+	
+	private String name;
+	
+	private Date introduced;
+	
+	private Date discontinued;
+	
+	private long idCompany;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getIntroduced() {
+		return introduced;
+	}
+
+	public void setIntroduced(Date introduced) {
+		this.introduced = introduced;
+	}
+
+	public Date getDiscontinued() {
+		return discontinued;
+	}
+
+	public void setDiscontinued(Date discontinued) {
+		this.discontinued = discontinued;
+	}
+	
+	
+}
